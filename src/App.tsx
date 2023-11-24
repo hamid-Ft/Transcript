3e8ac762edd7d6d2a7bd0a3105ca9a53b49c5adc
HamidFt <hamidfattahi.a@gmail.com>
2023-11-24 20:44:00 +0330

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import './App.css';
-const hamid = 'ss';
+
 function App() {
+    const [text, setText] = useState('second');
+
     return (
         <>
-            <h1>Hello</h1>
-            <h1>Hello</h1>
+            <h1>{text}</h1>
+            <button
+                type="button"
+                onClick={() => {
+                    setText('oh yeah');
+                }}
+            >
+                <h1>{text}</h1>
+            </button>
             <h1>Hello</h1>
             <h1>Hello</h1>
         </>
